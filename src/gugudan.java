c90987af04f196d44bd3f0299bf119855d3b80dd
leejiseong <leejiseong@DESKTOP-AKI7KFF>
2021-01-22 03:48:02 +0900

    값 입력 및 조건문

--- src/gugudan.java
+++ src/gugudan.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 
 public class gugudan {
 
 	public static void main(String[] args) {
-		System.out.println("안녕하세요!");
-		System.out.println("안녕하세요!");
-
+		System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("number : " + number);
+        
+        if (number<2 || number>9) {
+        	System.out.println("잘못된 값입니다");
+        } else {
+        	for(int i=1 ; i<10 ; i++) {
+            	System.out.println(number+"*"+i+"="+(number*i));
+            }
+        }
+        
+	
+	
+	
 	}
 
 }
