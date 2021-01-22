989c45c38b08a3178af2ac3508d49ec1f79a48a4
leejiseong <leejiseong@DESKTOP-AKI7KFF>
2021-01-22 14:22:36 +0900

    배열을 활용

--- src/gugudan.java
+++ src/gugudan.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class gugudan {
 
 	public static void main(String[] args) {
+		int[] result = new int[9];
 		System.out.println("구구단 중 출력할 단은? : ");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
@@ -11,9 +12,13 @@ public class gugudan {
         if (number<2 || number>9) {
         	System.out.println("잘못된 값입니다");
         } else {
-        	for(int i=1 ; i<10 ; i++) {
-            	System.out.println(number+"*"+i+"="+(number*i));
+        	for(int i=0 ; i<9 ; i++) {
+            	result[i] = number *(i+1);
+        		
             }
+        	for (int i=0 ; i<9 ; i++) {
+        		System.out.println(number+"*"+(i+1)+"="+(result[i]));
+        	}
         }
         
 	
