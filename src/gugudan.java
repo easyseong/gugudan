eba25031090226c220ed0e5cce32076a11dd34a2
leejiseong <leejiseong@DESKTOP-AKI7KFF>
2021-01-22 14:31:49 +0900

    메소드를 활용

--- src/gugudan.java
+++ src/gugudan.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
 
 public class gugudan {
+	public static void calculate(int num) {
+		int[] result = new int[9];
+		if (num<2 || num>9) {
+        	System.out.println("잘못된 값입니다");
+        } else {
+        	for(int i=0 ; i<9 ; i++) {
+            	result[i] = num *(i+1);
+        		
+            }
+        	for (int i=0 ; i<9 ; i++) {
+        		System.out.println(num+"*"+(i+1)+"="+(result[i]));
+        	}
+        }
+        
+	}
 
 	public static void main(String[] args) {
-		int[] result = new int[9];
+		
 		System.out.println("구구단 중 출력할 단은? : ");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         System.out.println("number : " + number);
+        calculate(number);
         
-        if (number<2 || number>9) {
-        	System.out.println("잘못된 값입니다");
-        } else {
-        	for(int i=0 ; i<9 ; i++) {
-            	result[i] = number *(i+1);
-        		
-            }
-        	for (int i=0 ; i<9 ; i++) {
-        		System.out.println(number+"*"+(i+1)+"="+(result[i]));
-        	}
-        }
         
 	
 	
